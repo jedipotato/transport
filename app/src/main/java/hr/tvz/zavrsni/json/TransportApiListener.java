7048e8c8a319685d31caee45d4dc421a23d75253
Josip Marković <josip.markovic09@gmail.com>
2015-02-09 15:51:33 +0100

    fixed minor issue with ApiListener

--- app/src/main/java/hr/tvz/zavrsni/json/TransportApiListener.java
+++ app/src/main/java/hr/tvz/zavrsni/json/TransportApiListener.java
@@ -1,9 +1,11 @@
 package hr.tvz.zavrsni.json;
 
+import hr.tvz.zavrsni.domain.api.BasicModel;
+
 /**
  * Sučelje odgovorno za slušanje na odaziv API odgovora
  */
-public interface TransportApiListener<T> {
+public interface TransportApiListener<T extends BasicModel> {
 
     void onApiResponse(T response);
     void onApiFailure();
