b73369e8194d7964f86d7ce6e013a47a56dead0b
Josip Marković <josip.markovic09@gmail.com>
2015-02-09 23:57:01 +0100

    switching between modes in profile

--- app/src/main/java/hr/tvz/zavrsni/transportapplication/ProfileActivity.java
+++ app/src/main/java/hr/tvz/zavrsni/transportapplication/ProfileActivity.java
@@ -4,6 +4,7 @@ package hr.tvz.zavrsni.transportapplication;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import hr.tvz.zavrsni.domain.api.BasicModel;
 import hr.tvz.zavrsni.domain.api.User;
@@ -18,9 +19,13 @@ public class ProfileActivity extends TransportActivity {
     private EditText mEditTextPassword;
     private EditText mEditTextNewPassword;
     private EditText mEditTextRepeatNewPassword;
+    private TextView mTextViewPassword;
+    private TextView mTextViewNewPassword;
+    private TextView mTextViewRepeatNewPassword;
 
     private App mApp;
     private User mUser;
+    private boolean mIsInEditMode = false;
 
     private TransportApiListener<User> mGetProfileApiListener = new TransportApiListener<User>() {
         @Override
@@ -56,6 +61,22 @@ public class ProfileActivity extends TransportActivity {
         mEditTextEmail.setText(user.getEmail());
     }
 
+    private void switchMode(boolean isEditMode) {
+        mEditTextName.setEnabled(isEditMode);
+        mEditTextSurname.setEnabled(isEditMode);
+        mEditTextUsername.setEnabled(isEditMode);
+        mEditTextEmail.setEnabled(isEditMode);
+        mEditTextPassword.setEnabled(isEditMode);
+        mEditTextPassword.setVisibility(isEditMode ? View.VISIBLE : View.GONE);
+        mEditTextNewPassword.setEnabled(isEditMode);
+        mEditTextNewPassword.setVisibility(isEditMode ? View.VISIBLE : View.GONE);
+        mEditTextRepeatNewPassword.setEnabled(isEditMode);
+        mEditTextRepeatNewPassword.setVisibility(isEditMode ? View.VISIBLE : View.GONE);
+        mTextViewPassword.setVisibility(isEditMode ? View.VISIBLE : View.GONE);
+        mTextViewNewPassword.setVisibility(isEditMode ? View.VISIBLE : View.GONE);
+        mTextViewRepeatNewPassword.setVisibility(isEditMode ? View.VISIBLE : View.GONE);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -70,6 +91,9 @@ public class ProfileActivity extends TransportActivity {
         mEditTextPassword = (EditText) findViewById(R.id.profileInputPassword);
         mEditTextNewPassword = (EditText) findViewById(R.id.profileInputNewPassword);
         mEditTextRepeatNewPassword = (EditText) findViewById(R.id.profileInputRepeatNewPassword);
+        mTextViewPassword = (TextView) findViewById(R.id.profileTextPassword);
+        mTextViewNewPassword = (TextView) findViewById(R.id.profileTextNewPassword);
+        mTextViewRepeatNewPassword = (TextView) findViewById(R.id.profileTextRepeatNewPassword);
     }
 
     @Override
@@ -81,5 +105,21 @@ public class ProfileActivity extends TransportActivity {
     }
 
     public void onClickUpdate(View view) {
+        if (!mIsInEditMode) {
+            mIsInEditMode = true;
+            switchMode(true);
+        } else {
+            //TODO api
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mIsInEditMode) {
+            mIsInEditMode = false;
+            switchMode(false);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
